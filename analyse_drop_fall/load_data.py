Fix distance calculation
Distances should be reported as the distances from the center of
edge pixels rather than the edges of those pixels

analyse_drop_fall/load_data.py
@@ -172,8 +172,9 @@ def compute_droplet_values_single_frame(threshold_frame, starty, config):
 
     while True:
         line = threshold_frame[y_coord, :]
-        width = np.count_nonzero(line)
-        if width == 0:
+        # measure pixel to pixel distance
+        width = np.count_nonzero(line) - 1
+        if width == -1:
             # no longer counting the actual size
             break
         if width > max_width:
@@ -217,7 +218,7 @@ def compute_droplet_values_single_frame(threshold_frame, starty, config):
     volume_cone += dV_conical
 
     # full length of the droplet
-    length = starty - y_coord
+    length = starty - y_coord - 1
 
     # other volume option -> model as an ellipsoid
 

analyse_drop_fall/main.py
@@ -70,46 +70,6 @@ def find_dynamic_max_spread(contact_widths, pre_impact_frame):
             pre_impact_frame:pre_impact_frame+15]) + pre_impact_frame
 
 
-def post_impact_analysis(full_frames_data, pre_impact_frame, config):
-    # find contact line (equation)
-
-    # do this based on a number of frames past the pre impact frame
-    a, b = find_equation_of_contact_line(
-        full_frames_data, pre_impact_frame, config)
-
-    # cut off the image below this
-
-    contact_widths = [0] * pre_impact_frame
-    post_impact_data = ()
-    reflection_removed = []
-    for frame in clean_thresh_frames:
-        contact_widths.append(find_contact_width(frame, a, b))
-        # update the threshold frames
-        # recompute solidity, convex outline, height
-        cleaner_thresh = clean_reflection(frame[:, :], a, b)
-        reflection_removed.append(reflection_removed)
-
-    # track contact line dynamics (width change)
-
-    contact_widths = np.array(contact_widths) * config.PIXELS_TO_MM
-
-    # remove the
-
-    # track height dynamics
-
-    # find max contact line frame
-    # find max width frame
-    # compute solidity for max contact line frame and max width frame
-    # shape analysis
-
-    # times between impact and
-    # - max spread, max width, max height, heigh stability(? how?)
-    # spread returns to max spread
-    # is the final spread max spread ?
-
-    pass
-
-
 def get_summary_statistics(full_frames_data, config):
     # sumarise:
     # solidity at max spread
@@ -131,7 +91,8 @@ def post_impact_convex_analysis(reflection_cleaned_frame, config):
                              method=cv2.CHAIN_APPROX_SIMPLE)
 
     # set up the 'ConvexImage' bit of regionprops.
-    convex_frame = np.zeros(reflection_cleaned_frame.shape[0:2]).astype('uint8')
+    convex_frame = np.zeros(
+        reflection_cleaned_frame.shape[0:2]).astype('uint8')
 
     # it is tecnhically possible to get more than one object here
     # if e.g. the droplet splits apart
@@ -149,7 +110,7 @@ def post_impact_convex_analysis(reflection_cleaned_frame, config):
     cv2.drawContours(convex_frame, [convex_hull], -1,
                      color=1, thickness=-1)
 
-    height = cv2.boundingRect(c)[3] * config.PIXELS_TO_METERS
+    height = (cv2.boundingRect(c)[3] - 1) * config.PIXELS_TO_METERS
 
     return convex_frame, solidity, height
 